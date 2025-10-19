b72fb6d45827d68257f2665f7df994fa4696cad2
luowei <frwalker-c@qq.com>
2025-10-19 21:25:14 +0800

    只打印一次hexo env

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 'use strict';
 
 
+// 打印 Hexo 环境信息到终端
+console.log('========== Hexo Environment ==========');
+console.log('hexo.env:', hexo.env);
+console.log('====================================');
 hexo.extend.helper.register('cdn_css', require('./lib/helper').cdn_css);
 hexo.extend.helper.register('cdn_js', require('./lib/helper').cdn_js);
 hexo.extend.helper.register('cdn_asset', require('./lib/helper').cdn_asset);
